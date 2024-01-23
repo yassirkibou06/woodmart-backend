ad27e7ae2df9aa0fc07226c3ce61248118e9fad8
Yassir Kibou <102799967+yassirkibou06@users.noreply.github.com>
2024-01-23 23:47:25 +0100

    vooo

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,19 +1,19 @@
-/*const express = require('express');
+const express = require('express');
 const { createProduct, getaProduct, getAllProducts, updateaProduct, deleteaProduct, addToWishlist, rating, uploadImages } = require('../controller/productCtrl');
 const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
-const { uploadPhotos, productImageResize } = require('../middlewares/uploadImages');
+const { uploadPhotos, productImageResize } = require('../middlewares/uploadimages');
 
 const router = express.Router();
 
-router.post('/', authMiddleware, isAdmin, createProduct);
+/*router.post('/', authMiddleware, isAdmin, createProduct);
 router.put('/upload/:id', authMiddleware, isAdmin, uploadPhotos.array('images', 10), productImageResize, uploadImages);
 router.get('/:id', getaProduct);
 router.put('/whishlist', authMiddleware, addToWishlist);
 router.put('/rating', authMiddleware, rating);
 
 router.put('/:id',authMiddleware, isAdmin, updateaProduct);
-router.delete('/:id',authMiddleware, isAdmin, deleteaProduct);
+router.delete('/:id',authMiddleware, isAdmin, deleteaProduct);*/
 router.get('/', getAllProducts);
 
 
-module.exports = router*/
+module.exports = router;
