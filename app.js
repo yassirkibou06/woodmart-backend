b8dccd18e8695f54dcf1ce5a26bf019f769a934b
Yassir Kibou <102799967+yassirkibou06@users.noreply.github.com>
2024-01-23 23:09:49 +0100

    appop

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
-const dbConnect = require('../config/dbConnect');
+const dbConnect = require('./config/dbConnect');
 /*const authRoute = require('./routes/authRoute');
 const categoryRoute = require('./routes/categoryRoute');
 const brandRoute = require('./routes/brandRoute');
 const couponRoute = require('./routes/couponRoute');*/
-const productRoute = require('../routes/productRoute');
-const { notFound, errorHandler } = require('../middlewares/errorHandler');
+const productRoute = require('./routes/productRoute');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
