26a4f85f7a8556423c0a875f73206163ef444b07
Yassir Kibou <102799967+yassirkibou06@users.noreply.github.com>
2024-01-23 18:24:17 +0100

    node 5

--- index.js
+++ index.js
@@ -13,11 +13,6 @@ const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 
-dbConnect();
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.get('/', (req, res) => res.send('Home Page Route'));
 
@@ -31,6 +26,12 @@ app.use("/api/coupon", couponRoute);
 app.use(notFound);
 app.use(errorHandler);
 
+
+dbConnect();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
