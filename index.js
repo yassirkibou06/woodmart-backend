c78cb4b19443d754e51f854d59a2d8d6177ef738
Yassir Kibou <102799967+yassirkibou06@users.noreply.github.com>
2024-01-23 21:40:29 +0100

    01

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 */
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port`);
 })
 
 /*"server": "nodemon index.js"**/
