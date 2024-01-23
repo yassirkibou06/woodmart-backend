4b6845b7afd371420ef3f3238321ea5b651879ff
Yassir Kibou <102799967+yassirkibou06@users.noreply.github.com>
2024-01-23 23:55:48 +0100

    hghgh

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
 const dbConnect = require('./config/dbConnect');
-/*const authRoute = require('./routes/authRoute');
+const authRoute = require('./routes/authRoute');
 const categoryRoute = require('./routes/categoryRoute');
 const brandRoute = require('./routes/brandRoute');
-const couponRoute = require('./routes/couponRoute');*/
+const couponRoute = require('./routes/couponRoute');
 const productRoute = require('./routes/productRoute');
 const { notFound, errorHandler } = require('./middlewares/errorHandler');
 const cookieParser = require('cookie-parser');
@@ -14,13 +14,13 @@ const bodyParser = require('body-parser');
 
 app.get('/', (req, res) => res.send('Home Page Route'));
 
-//app.use("/api/user", authRoute);
+app.use("/api/user", authRoute);
 app.use("/api/product", productRoute);
-/*app.use("/api/category", categoryRoute);
+app.use("/api/category", categoryRoute);
 app.use("/api/brand", brandRoute);
 app.use("/api/coupon", couponRoute);
 
-*/
+
 app.use(notFound);
 app.use(errorHandler);
 
