d0f43790b5dd5f945bc3cc8751591fccdcfaeef9
Yassir Kibou <102799967+yassirkibou06@users.noreply.github.com>
2024-01-23 17:44:01 +0100

    89

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const app = express();
 const dotenv = require('dotenv').config();
 const dbConnect = require('./config/dbConnect');
 const PORT = process.env.PORT || 3000;
+const authRoute = require('./routes/authRoute');
+const productRoute = require('./routes/productRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const brandRoute = require('./routes/brandRoute');
+const couponRoute = require('./routes/couponRoute');
+const bodyParser = require('body-parser');
+const { notFound, errorHandler } = require('./middlewares/errorHandler');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+
+dbConnect();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => res.send('Home Page Route'));
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 
 app.listen(PORT, () => {
